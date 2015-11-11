958ad99c183737cd7eedc59375c900982ed8d84c
liscju <piotr.listkiewicz@gmail.com>
2015-11-11 20:22:42 +0100

    Add MultiSwarmParallel class to swarm package

---
+++ src/pl/edu/agh/miss/swarm/MultiSwarmParallel.java
@@ -0,0 +1,12 @@
+package pl.edu.agh.miss.swarm;
+
+import net.sourceforge.jswarm_pso.FitnessFunction;
+
+public class MultiSwarmParallel extends MultiSwarm {
+
+    public MultiSwarmParallel(SwarmInformation[] swarmInfos, FitnessFunction fitnessFunction) {
+        super(swarmInfos, fitnessFunction);
+    }
+
+
+}
