43aad4e99461dc11482464e3776b8cf621ea00c8
liscju <piotr.listkiewicz@gmail.com>
2015-11-11 20:30:28 +0100

    Copy evaluate() function from Swarm class to MultiSwarmParallel

--- src/pl/edu/agh/miss/swarm/MultiSwarmParallel.java
+++ src/pl/edu/agh/miss/swarm/MultiSwarmParallel.java
@@ -8,5 +8,42 @@ public class MultiSwarmParallel extends MultiSwarm {
         super(swarmInfos, fitnessFunction);
     }
 
+    /**
+     * Evaluate fitness function for every particle
+     * Warning: particles[] must be initialized and fitnessFunction must be set
+     */
+    public void evaluate() {
+        if (particles == null) throw new RuntimeException("No particles in this swarm! May be you need to call Swarm.init() method");
+        if (fitnessFunction == null) throw new RuntimeException("No fitness function in this swarm! May be you need to call Swarm.setFitnessFunction() method");
 
+        // Initialize
+        if (Double.isNaN(bestFitness)) {
+            bestFitness = (fitnessFunction.isMaximize() ? Double.NEGATIVE_INFINITY : Double.POSITIVE_INFINITY);
+            bestParticleIndex = -1;
+        }
+
+        //---
+        // Evaluate each particle (and find the 'best' one)
+        //---
+        for (int i = 0; i < particles.length; i++) {
+            // Evaluate particle
+            double fit = fitnessFunction.evaluate(particles[i]);
+
+            numberOfEvaliations++; // Update counter
+
+            // Update 'best global' position
+            if (fitnessFunction.isBetterThan(bestFitness, fit)) {
+                bestFitness = fit; // Copy best fitness, index, and position vector
+                bestParticleIndex = i;
+                if (bestPosition == null) bestPosition = new double[sampleParticle.getDimension()];
+                particles[bestParticleIndex].copyPosition(bestPosition);
+            }
+
+            // Update 'best neighborhood'
+            if (neighborhood != null) {
+                neighborhood.update(this, particles[i]);
+            }
+
+        }
+    }
 }
