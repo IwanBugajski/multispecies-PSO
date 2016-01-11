41801ab306a87ea5607f6d711d1c8255458024dc
liscju <piotr.listkiewicz@gmail.com>
2016-01-11 08:48:17 +0100

    Adds Schwefel fitness function

---
+++ src/pl/edu/agh/miss/fitness/Schwefel.java
@@ -0,0 +1,29 @@
+package pl.edu.agh.miss.fitness;
+
+import net.sourceforge.jswarm_pso.FitnessFunction;
+
+import static java.lang.Math.abs;
+import static java.lang.Math.sin;
+import static java.lang.Math.sqrt;
+
+/**
+ * Schwefel function according to description at:
+ * http://www.sfu.ca/~ssurjano/schwef.html
+ */
+public class Schwefel extends FitnessFunction{
+
+    @Override
+    public double evaluate(double[] position) {
+        double d = position.length;
+        double sumOfXSinSqrtX = calculateSumOfXSinSqrtX(position);
+        return 418.9829 * d - sumOfXSinSqrtX;
+    }
+
+    private double calculateSumOfXSinSqrtX(double[] position) {
+        double result = 0;
+        for (double pos : position) {
+            result += pos * sin(sqrt(abs(pos)));
+        }
+        return result;
+    }
+}
